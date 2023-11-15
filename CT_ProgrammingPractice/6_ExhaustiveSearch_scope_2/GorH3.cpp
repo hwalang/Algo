8a1a7ebc18a2391a923cc2c4513e1caa6582c053
hwalang <smdong97@gmail.com>
2023-11-15 09:45:55 +0900

    20231115_CT_GorH3

---
+++ CT_ProgrammingPractice/6_ExhaustiveSearch_scope_2/GorH3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_L 10000
+
+int N, K, ans;
+int loc[MAX_L + 1];
+
+int main() {
+
+	cin >> N >> K;
+	int cur_idx; char alpha;
+	for (int i = 0; i < N; ++i) {
+		cin >> cur_idx >> alpha;
+		if (alpha == 'G')
+			loc[cur_idx] = 1;
+		else
+			loc[cur_idx] = 2;
+	}
+	// 구간 합 구하기
+	K++;
+	for (int i = 1; i <= MAX_L - K + 1; ++i) {
+		int sum = 0;
+		for (int j = i; j < i + K; ++j)
+			sum += loc[j];
+		ans = max(ans, sum);
+	}
+
+	cout << ans;
+
+	return 0;
+}
