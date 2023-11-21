c347eea44c44a825af8270d7e8f6e869ca99df76
hwalang <smdong97@gmail.com>
2023-11-22 00:19:39 +0900

    20231122_CT_아름다운수열2

--- CT_ProgrammingPractice/6_ExhaustiveSearch_scope_4/아름다운수열2.cpp
+++ CT_ProgrammingPractice/6_ExhaustiveSearch_scope_4/아름다운수열2.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 #define MAX_N 100
 #define MAX_M 100
 
-int N, M;
-int A[MAX_N], B[MAX_M];
+int N, M, ans;
+int A[MAX_N], B[MAX_M], temp[MAX_N];
 
 int main() {
 	cin >> N >> M;
@@ -14,12 +15,27 @@ int main() {
 	for (int i = 0; i < M; ++i)
 		cin >> B[i];
 
-	// B가 만들 수 있는 경우의 수를 기준
-	for (int i = 0; i < M; ++i) {
-		// for (int j = i + 1; )
+	// 정렬을 통해 수열을 비교한다.
+	sort(B, B + M);
+	for (int i = 0; i <= N - M; ++i) {
+		// A의 
+		for (int j = 0; j < M; ++j)
+			temp[j] = A[i + j];
+		sort(temp, temp + M);
+
+		bool flag = true;
+		for (int j = 0; j < M; ++j) {
+			if (temp[j] != B[j]) {
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag)
+			ans++;
 	}
 
-
+	cout << ans;
 
 	return 0;
 }
